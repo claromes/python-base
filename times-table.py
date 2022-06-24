493850af30e86973c0c49044d667cfe67fa060a8
Claromes <claromes@hey.com>
2022-06-23 23:27:09 -0300

    formating times tables

--- times-table.py
+++ times-table.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 """Print times table 1 to 10."""
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__  = "claromes"
 
-# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 numbers = list(range(1, 11))
 
-# Iterable
-for number in numbers:
-	print(number, "Times table")
-	for other_number in numbers:
-	    print(number * other_number)
-	print("--------")
+print("{:^23}".format("times tables\n".upper()))
+
+for number_1 in numbers:
+    print("{:^23}".format(f"{number_1} Times Table \N{Abacus}\n"))
+
+    for number_2 in numbers:
+        result = number_1 * number_2
+        print("{:^23}".format(f"{number_1} x {number_2} = {result}"))
+
+    print("." * 23)
